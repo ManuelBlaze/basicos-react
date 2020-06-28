fe45f6ac734d149293e0a72b69da0177d615d5c7
ManuelBlaze <manuel.escobar1217@gmail.com>
2020-06-28 18:12:08 -0500

    Tienda online

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 
-function Header() {
-    
+function Header({titulo}) {
     return(
-        <h1 id="encabezado" className="encabezado">Tienda virtual</h1>
+        <h1 className="encabezado">{titulo}</h1>
     )
 }
 
